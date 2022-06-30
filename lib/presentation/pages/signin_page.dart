89ea14aaa48b6be56c56d435898315aa5c7b6979
Tani1015 <1015ryusuke1015@gmail.com>
2022-06-30 11:05:43 +0900

    add firebase

--- lib/presentation/pages/signin_page.dart
+++ lib/presentation/pages/signin_page.dart
@@ -1,1 +1,29 @@
-//signin
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final emailTextProvider = Provider.autoDispose((ref) {
+  return TextEditingController(text: '');
+});
+
+final passTextProvider = Provider.autoDispose((ref) {
+  return TextEditingController(text: '');
+});
+
+class SigninPage extends HookConsumerWidget{
+  const SigninPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final emailTextController = ref.watch(emailTextProvider);
+    final passTextController = ref.watch(passTextProvider);
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+
+      ),
+    );
+  }
+
+}
